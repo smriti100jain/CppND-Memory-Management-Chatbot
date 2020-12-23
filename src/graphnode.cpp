03e40c0ea46a6af4ae87f4161574e2263686e7e7
smriti100jain <smriti100jain@gmail.com>
2020-12-23 15:49:25 +0000

    working version

---
+++ src/graphnode.cpp
@@ -0,0 +1,62 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    //delete _chatBot; 
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+{
+    _childEdges.push_back(std::move(edge)); 
+  //unique_ptr guarantees that a single unique_ptr container has ownership of the held pointer.
+  //This means that you can't make copies of a unique_ptr (because then two unique_ptrs would have ownership), so you can only move it.
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
+{
+    _chatBot = std::move(chatbot);
+    _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(_chatBot);
+//     _chatBot = nullptr; // invalidate pointer at source
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
